27ba9ce0c78dc3ef17a8506315c96da99d3a2fdb
HJoey <joey853853@gmail.com>
2021-06-28 14:38:57 +0800

    Add the code insetion target in the input verilog file.

--- src/ext-eco/eco_init.cpp
+++ src/ext-eco/eco_init.cpp
@@ -57,6 +57,7 @@ using namespace std;
 
 
 static int Abc_CommandRunEco_test(Abc_Frame_t* pAbc, int argc, char** argv);
+void insertTarget(string inFName, string oFName);
 
 
 void init(Abc_Frame_t* pAbc) {
@@ -269,5 +270,189 @@ int Abc_CommandRunEco_test(Abc_Frame_t* pAbc, int argc, char** argv) {
   //fwrite( wire1 /n wire2 /n ..... ,nFile)
   //fclose( nFile );
   //fclose( gFile );
+
+
+  //////////// insert target ////////////
+  cout << "starts to insert target" << endl;
+  insertTarget("F.v","F_t.v");
+
+
+
   return 0;
+}
+
+vector<string>  split(const string& str,const string& delim) {
+    vector<string> res;
+    if("" == str) return  res;
+    
+    string strs = str + delim;
+    size_t pos;
+    size_t size = strs.size();
+ 
+    for (int i = 0; i < size; ++i) {
+        pos = strs.find(delim, i);
+        if( pos < size) {
+            string s = strs.substr(i, pos - i);
+            res.push_back(s);
+            i = pos + delim.size() - 1;
+        }
+        
+    }
+    return res; 
+}
+
+
+string& trim(string &s, string end=" ") 
+{
+    if (s.empty()) 
+    {
+        return s;
+    }
+    s.erase(0,s.find_first_not_of(" "));
+    s.erase(s.find_last_not_of(end) + 1);
+    return s;
+}
+
+void insertTarget(string inFName, string oFName="F.v")
+{
+    fstream fin, fout;
+    string buf;
+    
+    vector<string> allWire;
+    fin.open("wirename",ios::in);
+    while(!fin.eof())
+    {
+        fin >> buf;
+        allWire.push_back(buf);
+    }
+    fin.close();
+
+    set<string> allTarget;
+    fin.open("wirename",ios::in);
+    while(!fin.eof())
+    {
+        fin >> buf;
+        allTarget.insert(buf);
+    }
+    fin.close();
+
+    // cout << "allWire contains:\n";
+    // for (auto& s : allWire)
+    //     cout << s << ',';
+    // cout << endl << allWire.size() << endl;
+
+    fin.open(inFName,ios::in);
+    fout.open(oFName,ios::out);
+    string line;
+    vector<string>  splited;
+    set<string> oNames, iNames;
+    set<string> declaredWire;
+    while(getline(fin,line))
+    {
+        cout << line << endl;
+        if(line.find("module")==0 || line.find("endmodule")==0)
+        {
+            fout << line << endl;
+            continue;
+        }
+        if(line.find("wire")==0)
+            continue;
+        if(line.find("output")==0)
+        {
+            splited = split(line," ");
+            for(int i=1;i<splited.size();i++)
+            {
+                trim(splited[i],";\n");
+                trim(splited[i],",");
+                oNames.insert(splited[i]);
+            }
+            fout << line << endl;
+        }
+        else if(line.find("input")==0)
+        {
+            splited = split(line," ");
+            for(int i=1;i<splited.size();i++)
+            {
+                trim(splited[i],";\n");
+                trim(splited[i],",");
+                iNames.insert(splited[i]);
+            }
+            fout << line << endl;
+        }
+        else
+        {
+            string gate = line.substr(0,line.find(" "));
+            //cout << "gate: " << gate << endl;
+            size_t st=line.find("("), end=line.find(")");
+            string wireStr = line.substr(st+1,end-st-1);
+            trim(wireStr);
+            cout << "wireStr: " << wireStr << endl;
+            splited = split(wireStr," ");
+            for(int i=0;i<splited.size();i++)//for each pin of a gate
+            {
+                trim(splited[i],",");
+                if(splited[i]=="")
+                    continue;
+                cout << i << ": " << splited[i] << endl;
+                if(i==0)//gate output
+                {
+                    //the pin is PO and is target
+                    if(oNames.find(splited[i])!=oNames.end() && allTarget.find(splited[i])!=allTarget.end())
+                    {
+                        cout << "outputs: " << splited[i] << endl;
+                        fout << "wire t_" << splited[i] << ", u_" << splited[i] << ";\n";
+                        fout << "buf (" << splited[i] << ",t_" << splited[i] << ");\n";
+                        splited[i] = "u_"+splited[i];
+                    }
+                    //the pin is internal wire and is target
+                    else if(allTarget.find(splited[i])!=allTarget.end())
+                    {
+                        cout << "wires: " << splited[i] << endl;
+                        fout << "wire u_" << splited[i] << ";\n";
+                        splited[i] = "u_"+splited[i];
+                    }
+                    //the pin isn't a target
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else//gate input
+                {
+                    if(allTarget.find(splited[i])!=allTarget.end())
+                    {
+                        cout << "inputs: " << splited[i] << endl;
+                        if(declaredWire.find(splited[i])==declaredWire.end())
+                        {
+                            fout << "wire t_" << splited[i] << ";" << endl;
+                            declaredWire.insert(splited[i]);    
+                        }
+                        splited[i] = "t_"+splited[i];
+                    }
+                    //the pin isn't a target
+                    else
+                    {
+                        continue;
+                    }
+                }
+                
+            }
+            fout << gate << " (";
+            for(int i=0;i<splited.size();i++)
+            {
+                if(splited[i]!="")
+                {
+                    fout << splited[i];
+                    if(i<splited.size()-1)
+                        fout << ", ";
+                    else
+                        fout << ");\n";
+                }
+                
+            }
+
+        }
+    }
+    fin.close();
+    fout.close();
 }
